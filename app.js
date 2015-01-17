cccfd5aba4fcc34d8160e6fa9fe460290e880c01
Kendra <kdodds@gmail.com>
2015-01-16 17:27:32 -0700

    mustache and json added to player

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+$( document ).ready(function() {
+
+$('.container').on('click', '.fa-play', function () {
+  $('.fa').removeClass('fa-pause').addClass('fa-play');
+  $(this).removeClass('fa-play').addClass('fa-pause');
+});
+
+$('.container').on('click', '.fa-pause', function() {
+  $(this).removeClass('fa-pause').addClass('fa-play');
+});
+
+
+  jQuery.getJSON('data.json', function(tracks) {
+
+    var $tracksTemplate = $('#tracksTemplate').html();
+
+    var newHTML = Mustache.to_html($tracksTemplate, tracks);
+
+    $('.player').html(newHTML);
+  });
+
+
+
+});
