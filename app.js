b348ed61c4dff605730fb5c4549d1a1db77b82c9
Kendra <kdodds@gmail.com>
2015-02-01 16:21:58 -0700

    music toggling and playing with titles

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 "use strict";
 
 // (Path, Callback)
-
 $( document ).ready(function() {
-
   $.getJSON('data.json', function(tracks) {
-    console.log("json received");
-
     var $tracksTemplate = $('#tracksTemplate').html();
     var newHTML = Mustache.to_html($tracksTemplate, tracks);
     $('.player').html(newHTML);
   });
 
-  console.log("after getJSON");
-
   $('.player').on('click', '.fa-play', function () {
-
     $('.fa-stop').removeClass('fa-stop').addClass('fa-play');
-
-    // `this` refers to the actual HTML element the user clicked on...
     $(this).removeClass('fa-play').addClass('fa-stop');
-    // var music_counter = $(this).closest("article div h2").attr("id") -1;
-    // $('audio').get(music_counter).play();
+
+    var title = 'Now playing: <em>' + $(this).attr('data-title') + '</em>';
+    $('.playerHeader h2').html(title);
+
+    var file = 'tracks/' + $(this).attr('data-file') + '.mp3';
+    $('audio').attr('src', file);
+    $('audio')[0].play();
   });
 
-  console.log("after on Play");
 
   $('.player').on('click', '.fa-stop', function() {
     $(this).removeClass('fa-stop').addClass('fa-play');
-    var music_counter = $(this).closest("article div h2").attr("id") -1;
-    $('audio').get(music_counter).pause();
-    console.log($('audio'));
+
+    var title = 'Select a song!';
+    $('.playerHeader h2').html(title);
+
+    $('audio')[0].pause();
   });
 
-  console.log("after on Stop");
-
 });
-
-console.log("after on Ready");
