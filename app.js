cadab9928709173969be8c27770446d96f64fff8
Kendra <kdodds@gmail.com>
2015-02-01 16:02:20 -0700

    study session with Jeff for toggle play buttons working

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 "use strict";
 
-// make an AJAX call to data.json
-jQuery.getJSON('data.json', function(tracks) {
-  var $tracksTemplate = $('#tracksTemplate').html();
-  var newHTML = Mustache.to_html($tracksTemplate, tracks);
-  $('.player').html(newHTML);
-});
+// (Path, Callback)
 
 $( document ).ready(function() {
-  $('.player').on('click', '.fa-play', function () {
-    $(this).removeClass('fa-play').addClass('fa-stop');
-    var music_counter = $(this).closest("article div h2").attr("id") -1;
-    $('audio').get(music_counter).play();
+
+  $.getJSON('data.json', function(tracks) {
+    console.log("json received");
+
+    var $tracksTemplate = $('#tracksTemplate').html();
+    var newHTML = Mustache.to_html($tracksTemplate, tracks);
+    $('.player').html(newHTML);
   });
+
+  console.log("after getJSON");
+
+  $('.player').on('click', '.fa-play', function () {
+
+    $('.fa-stop').removeClass('fa-stop').addClass('fa-play');
+
+    // `this` refers to the actual HTML element the user clicked on...
+    $(this).removeClass('fa-play').addClass('fa-stop');
+    // var music_counter = $(this).closest("article div h2").attr("id") -1;
+    // $('audio').get(music_counter).play();
+  });
+
+  console.log("after on Play");
 
   $('.player').on('click', '.fa-stop', function() {
     $(this).removeClass('fa-stop').addClass('fa-play');
@@ -21,4 +33,8 @@ $( document ).ready(function() {
     console.log($('audio'));
   });
 
+  console.log("after on Stop");
+
 });
+
+console.log("after on Ready");
